6f473d65224ec9ffa685a2d50518a562a84b4827
SeyyedMahdi <SeyyedMahdiHP@gmail.com>
2016-08-01 00:35:01 +0430

    درست عمل میکنه منتها نه تو همه شرایط

---
+++ check_for_balanced_parentheses_in_an_expression.py
@@ -0,0 +1,42 @@
+# in the name of God
+# Seyyed Mahdi HassanPour
+# SeyyedMahdiHP@gmail.com
+"""
+Given an expression string exp, write a program to examine whether
+ the pairs and the orders of “{“,”}”,”(“,”)”,”[“,”]” are correct in exp.
+  For example, the program should print true for exp = “[()]{}{[()()]()}” and
+   false for exp = “[(])”
+"""
+
+
+def parentheses_balance_checker(userinput):
+    length = len(userinput)
+    if not (0 == length % 2):
+        return False
+    open_exp = "[{("
+    close_exp = "]})"
+    stack_list = list()
+    index =0
+    for  item in  userinput:
+        if item in open_exp:
+            stack_list.append(item)
+            index += 1
+        elif item in close_exp:
+            if item == "}" and stack_list[index-1] == "{":
+                stack_list.pop()
+                index -=1
+            elif item == ")" and stack_list[index-1] == "(":
+                stack_list.pop()
+                index -= 1
+            elif item == "]" and stack_list[index-1] == "[":
+                stack_list.pop()
+                index -= 1
+            else:
+                return False
+        else:
+            return False
+    return True
+
+
+userinput = "[()]{}{[()()]()}"
+print(parentheses_balance_checker(userinput))
