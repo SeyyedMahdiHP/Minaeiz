1131a53650d2ac8db75992f3ccbdf2869cbf6e65
SeyyedMahdi <SeyyedMahdiHP@gmail.com>
2016-07-31 23:10:55 +0430

    calculate the sum correctly

--- largest_sum_contiguous_subarray.py
+++ largest_sum_contiguous_subarray.py
@@ -4,7 +4,7 @@
 """
 In computer science, the maximum subarray problem is the task of finding
 the contiguous subarray within a one-dimensional array of numbers which has the largest sum.
- For example, for the sequence of values −2, 1, −3, 4, −1, 2, 1, −5, 4;
+ For example, for the sequence of values [-2, 1, -3, 4, -1, 2, 1, -5, 4];
   the contiguous subarray with the largest sum is 4, −1, 2, 1, with sum 6.
 The problem was first posed by Ulf Grenander of Brown University in 1977,
 as a simplified model for maximum likelihood estimation of patterns in digitized images.
@@ -12,3 +12,16 @@ as a simplified model for maximum likelihood estimation of patterns in digitized
 https://en.wikipedia.org/wiki/Maximum_subarray_problem
 http://www.practice.geeksforgeeks.org/problem-page.php?pid=106
 """
+
+
+def largest_sum_contigues_subarray(array_list):
+    current_largest_subarray = final_largest_subarray = array_list[0]
+    start_index = 0
+    for i in range(1, len(array_list)):
+        current_largest_subarray = max(array_list[i], current_largest_subarray + array_list[i])
+        final_largest_subarray = max(final_largest_subarray, current_largest_subarray)
+    return [start_index, final_largest_subarray]
+
+
+input_value = [2, 3, -1, -20, 5, 10]  # [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(largest_sum_contigues_subarray(input_value))
