bccbbe8e7e4167d80303561b3decb23379d74911
SeyyedMahdi <SeyyedMahdiHP@gmail.com>
2016-08-20 14:53:03 +0430

    lottery new algs

--- 003-session/lottery.py
+++ 003-session/lottery.py
@@ -1,25 +1,52 @@
-from pprint import pprint as pp
+#in the name of GOD
+#SeyyedMahdiHassanpour
+#smahdi1991@gmail.com
+"""
+lottery algorithm
+this algorithm can be use in banking lottery
+each one has its own ...
+
+"""
 import random
-import collections
-import math
-min_score = 0
-max_score = 501
-multiple = 25
-
-scores = range(min_score, max_score, multiple)
-candidate = [scores.index(s) for s in scores for _ in range(s)]
-benchmarks = dict.fromkeys(candidate, 0)
-
-for _ in range(100000):
-    benchmarks[random.choice(candidate)] += 1
-print(collections.OrderedDict(sorted(benchmarks.items())))
+from collections import defaultdict
+scores = list(range(1, 500, 5))
+#print(scores)
 
 
-def f():
-    c = math.ceil(math.pow(random.random(), .5) * max(candidate))
-    return c
+def lottery1():
+    fair_list = [score for score in scores for _ in range(score)]
+    #print(fair_list)
+    return random.choice(fair_list)
 
-benchmarks = dict.fromkeys(candidate, 0)
-for _ in range(100000):
-    benchmarks[f()] += 1
-print(collections.OrderedDict(sorted(benchmarks.items())))
+#print(lottery1())
+
+d = defaultdict(int)  # we will see each score selection count
+for _ in range(10000):
+    d[lottery1()] += 1
+
+print(sorted(list(zip(d.keys(), d.values()))))
+# from pprint import pprint as pp
+# import random
+# import collections
+# import math
+# min_score = 0
+# max_score = 501
+# multiple = 25
+#
+# scores = range(min_score, max_score, multiple)
+# candidate = [scores.index(s) for s in scores for _ in range(s)]
+# benchmarks = dict.fromkeys(candidate, 0)
+#
+# for _ in range(100000):
+#     benchmarks[random.choice(candidate)] += 1
+# print(collections.OrderedDict(sorted(benchmarks.items())))
+#
+#
+# def f():
+#     c = math.ceil(math.pow(random.random(), .5) * max(candidate))
+#     return c
+#
+# benchmarks = dict.fromkeys(candidate, 0)
+# for _ in range(100000):
+#     benchmarks[f()] += 1
+# print(collections.OrderedDict(sorted(benchmarks.items())))
